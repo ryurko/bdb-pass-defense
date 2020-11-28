71dfdad2d14ceb56b3a020c4cbe42c7e06319b63
kpelechrinis <kostas.pelechrinis@gmail.com>
2020-11-28 11:42:30 -0500

    Update players_in_plays.R

--- kpele-scrap/players_in_plays.R
+++ kpele-scrap/players_in_plays.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(plyr)
 
 df <- list.files(pattern = ".rds") %>% map(readRDS) %>%  bind_rows() %>% mutate(key = paste0(as.character(df$gameId),"-",as.character(df$playId)))
-#x = ddply(df,~key,summarise, no_pl = length(unique(nflId)))
 
 offense <- c("QB","OLB","RB","TE","WR","FB","HB")
 defense <- c("CB","ILB","SS","DB","LB","MLB","FS","S","DT","DE","DL","NT")
@@ -10,3 +9,9 @@ defense <- c("CB","ILB","SS","DB","LB","MLB","FS","S","DT","DE","DL","NT")
 df$team_unit = ifelse(df$position %in% offense, "offense","defense")
 
 x = ddply(df,~key,summarise, no_pl = paste0(as.character(length(unique(nflId[team_unit == "offense"]))),"-",as.character(length(unique(nflId[team_unit == "defense"])))))
+
+# filter plays with complete pass 
+
+df_completed_pass_plays = df[(df$key %in% unique(df[df$event == 'pass_outcome_caught',]$key) &  ((df$event == "pass_outcome_caught") | (df$event== "pass_forward"))),]
+
+x = ddply(df_completed_pass_plays,~key,summarise, no_pl = paste0(as.character(length(unique(nflId[team_unit == "offense"]))),"-",as.character(length(unique(nflId[team_unit == "defense"])))))
