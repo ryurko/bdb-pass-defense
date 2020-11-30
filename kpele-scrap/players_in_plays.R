52c33383f754596e1e184c85fffcb03e68162903
kpelechrinis <kostas.pelechrinis@gmail.com>
2020-11-29 23:49:31 -0500

    Update players_in_plays.R

--- kpele-scrap/players_in_plays.R
+++ kpele-scrap/players_in_plays.R
@@ -1,14 +1,15 @@
 library(tidyverse)
 library(plyr)
 
-df <- list.files(pattern = ".rds") %>% map(readRDS) %>%  bind_rows() %>% mutate(key = paste0(as.character(df$gameId),"-",as.character(df$playId)))
+df <- list.files(pattern = ".rds") %>% map(readRDS) %>%  bind_rows() #%>% mutate(key = paste0(as.character(df$gameId),"-",as.character(df$playId)))
+df$key = paste0(as.character(df$gameId),"-",as.character(df$playId))
 
 offense <- c("QB","OLB","RB","TE","WR","FB","HB")
 defense <- c("CB","ILB","SS","DB","LB","MLB","FS","S","DT","DE","DL","NT")
 
 df$team_unit = ifelse(df$position %in% offense, "offense","defense")
 
-x = ddply(df,~key,summarise, no_pl = paste0(as.character(length(unique(nflId[team_unit == "offense"]))),"-",as.character(length(unique(nflId[team_unit == "defense"])))))
+#x = ddply(df,~key,summarise, no_pl = paste0(as.character(length(unique(nflId[team_unit == "offense"]))),"-",as.character(length(unique(nflId[team_unit == "defense"])))))
 
 # filter plays with complete pass 
 
